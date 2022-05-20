9e35ed68e67dd3e44ee6b60cbbb4800434008d40
Ayane1234 <ayane0703@gmail.com>
2022-05-20 10:27:17 +0900

    Areaの追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./App.css";
 import { Home } from "./screens/Home";
+import {
+  BrowserRouter,
+  Link,
+  Switch,
+  Route,
+  Routes,
+  Outlet,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="park-app">
-      <Home />
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/Home">Home</Link>
+        <br></br>
+        <Link to="/Playset">Playset</Link>
+        <br></br>
+        <Link to="/Age">Age</Link>
+        <br></br>
+        <Link to="/Area">Area</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Home } from "./screens/Home";
+import { Search } from "./screens/Playset";
+import { Age } from "./screens/Age";
+import { Area } from "./screens/Area";
+import { BrowserRouter, Link, Switch, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      {/* <React.StrictMode> */}
+      <Route path="/" element={<App />} />
+      <Route path="Home" element={<Home />} />
+      <Route path="Playset" element={<Search />} />
+      <Route path="Age" element={<Age />} />
+      <Route path="Area" element={<Area />} />
+      {/* </React.StrictMode> */}
+      <Route
+        path="*"
+        element={
+          <main
+            style={{
+              padding: "1rem",
+            }}
+          >
+            <p>There's nothing here!</p>
+          </main>
+        }
+      ></Route>
+    </Routes>
+  </BrowserRouter>
 );
