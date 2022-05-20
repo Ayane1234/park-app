3a325161a795167605d3e7d87179721297cce725
Ayane1234 <ayane0703@gmail.com>
2022-05-20 09:32:20 +0900

    年齢絞り込み画面の作成

---
+++ src/screens/Age.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Large } from "../components/Large";
+import { Header } from "../components/Header";
+import { Button } from "../components/Button";
+
+export const Age = () => {
+  return (
+    <div style={styles.body}>
+      <Header />
+      <section style={styles.section}>
+        <Large text="１−３歳" />
+        <Large text="３−６歳" />
+      </section>
+
+      <Button />
+    </div>
+  );
+};
+
+const styles = {
+  body: {
+    width: "100vw",
+    height: "100vh",
+    position: "relative",
+    backgroundColor: "#f3eed5",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 30,
+  },
+  section: {
+    width: "100%",
+    height: "auto",
+    display: "flex",
+    alignItems: "center",
+  },
+};
