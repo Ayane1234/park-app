532aedd1690659f93257331f8a341936fea8687a
Ayane1234 <ayane0703@gmail.com>
2022-07-30 08:45:35 +0900

    公園リストの、parksがlengthなしの場合の表示の切り替えを実装。件数の表示。（スタイルの修正は未完了）

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const SearchButton = ({ onClick, isSearch }) => {
-  console.log("props.isSearch:", isSearch);
+  // console.log("props.isSearch:", isSearch);
   return (
     <button
       style={isSearch ? styles.searchButtonTrue : styles.searchButtonFalse}
--- src/screens/ParkList.js
+++ src/screens/ParkList.js
@@ -17,7 +17,6 @@ export const ParkList = () => {
   // 公園詳細画面へのルーティングの設定
   const navigate = useNavigate();
   const toParkDetailInfo = (park) => {
-    // クリックした{park}を遷移先に渡す
     navigate("/ParkDetail", { state: { info: park } });
   };
 
@@ -60,13 +59,13 @@ export const ParkList = () => {
     });
   }, []);
 
-  // 地域フィルター関数
+  // 地域フィルター
   useEffect(() => {
     const areaFilterData = parks.filter((park) => park.area === areaName);
     setParks(areaFilterData);
   }, [areaName]);
 
-  // 年齢フィルター関数
+  // 年齢フィルター
   useEffect(() => {
     const { dataFilter } = location.state;
     if (dataFilter.includes("baby")) {
@@ -81,7 +80,7 @@ export const ParkList = () => {
     }
   }, [isAge]);
 
-  // 公園名フィルター関数
+  // 公園名フィルター
   useEffect(() => {
     const parkNameFilterData = parks.filter(
       (park) => park.name.match(parkName) || park.furigana.match(parkName)
@@ -127,7 +126,7 @@ export const ParkList = () => {
 
     setParks(playsetFilterData);
   };
-  // 遊具フィルター関数
+  // 遊具フィルター
   useEffect(() => {
     playsetFilterFunc();
   }, [playset]);
@@ -136,18 +135,33 @@ export const ParkList = () => {
   return (
     <div style={styles.body}>
       <Header2 />
+      {parks.length > 0 && (
+        <section style={styles.amountParkSection}>
+          <div style={styles.amountPark}>{parks.length}件</div>
+        </section>
+      )}
       <section style={styles.parkListSection}>
-        {parks.map((park, id) => {
-          return (
-            <ParkInfoCard
-              data={park}
-              onClick={() => {
-                toParkDetailInfo(park);
-              }}
-              key={id}
-            />
-          );
-        })}
+        {parks.length > 0 ? (
+          parks.map((park, id) => {
+            return (
+              <ParkInfoCard
+                data={park}
+                onClick={() => {
+                  toParkDetailInfo(park);
+                }}
+                key={id}
+              />
+            );
+          })
+        ) : (
+          <div style={styles.nonePark}>
+            <p style={styles.text}>
+              お探しの検索条件では、公園はありませんでした。
+              <br></br>
+              新しい検索条件で再検索してみてください。
+            </p>
+          </div>
+        )}
       </section>
     </div>
   );
@@ -156,15 +170,41 @@ export const ParkList = () => {
 const styles = {
   body: {
     width: "100vw",
-    minHeight: "100vh",
+    // height: "100%",
+    backgroundSize: "auto 100%",
+
+    // minHeight: "800px",
     position: "relative",
     backgroundColor: "#f3eed5",
-    display: "flex",
-    justifyContent: "center",
-    paddingTop: 60,
   },
   parkListSection: {
+    paddingTop: 30,
     width: "90%",
+    height: "800px",
+    margin: "auto",
+  },
+  // amountParkSection: {
+
+  // },
+  amountPark: {
+    width: "90%",
+
+    fontSize: "20px",
+    paddingTop: "60px",
+    margin: "0 0 0 80%",
+  },
+  nonePark: {
+    width: "300px",
     height: "auto",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    diplay: "flex",
+    height: "100vh",
+    fontSize: "20px",
+    textAlign: "center",
   },
 };
