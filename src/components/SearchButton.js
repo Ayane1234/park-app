4794156022da85ac5f69b1dc4e6058fd71e053dd
Ayane1234 <ayane0703@gmail.com>
2022-07-30 09:38:08 +0900

    さがすボタンの切り替えの確認（buttonタグ→divタグ）

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -1,38 +1,61 @@
 import React from "react";
 
 export const SearchButton = ({ onClick, isSearch }) => {
-  // console.log("props.isSearch:", isSearch);
+  console.log("props.isSearch:", isSearch);
   return (
-    <button
-      style={isSearch ? styles.searchButtonTrue : styles.searchButtonFalse}
+    <div
+      style={
+        isSearch === true ? styles.searchButtonTrue : styles.searchButtonFalse
+      }
       onClick={onClick}
     >
       さがす
-    </button>
+    </div>
   );
 };
 
 const styles = {
   searchButtonTrue: {
-    width: "221px",
-    height: "67px",
-    margin: "20px",
-    backgroundColor: "#d4dfbb",
+    width: "220px",
+    height: "100px",
+    margin: "30px",
+    backgroundColor: "#e4af9b",
+    color: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
     borderRadius: "30px",
-    border: "none",
-    fontSize: "18px",
-    position: "fixed",
-    bottom: 30,
+    // width: "221px",
+    // height: "67px",
+    // margin: "20px",
+    // backgroundColor: "#e4af9b",
+
+    // // backgroundColor: "#d4dfbb",
+    // borderRadius: "30px",
+    // border: "none",
+    // fontSize: "18px",
+    // position: "fixed",
+    // bottom: 30,
   },
   searchButtonFalse: {
-    width: "221px",
-    height: "67px",
-    margin: "20px",
-    backgroundColor: "#gray",
+    width: "220px",
+    height: "100px",
+    margin: "30px",
+    backgroundColor: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
     borderRadius: "30px",
-    border: "none",
-    fontSize: "18px",
-    position: "fixed",
-    bottom: 30,
+    // width: "221px",
+    // height: "67px",
+    // margin: "20px",
+    // backgroundColor: "#gray",
+    // borderRadius: "30px",
+    // border: "none",
+    // fontSize: "18px",
+    // position: "fixed",
+    // bottom: 30,
   },
 };
