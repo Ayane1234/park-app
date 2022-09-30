fd6ab33b113e282b1c694648937f363dbb64a5cf
Ayane1234 <ayane0703@gmail.com>
2022-10-01 08:53:46 +0900

    画像表示の実装

--- src/screens/ParkDetailInfo.js
+++ src/screens/ParkDetailInfo.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ParkBasicInfoSection } from "../components/ParkBasicInfoSection";
 import { Header } from "../components/Header";
 import { PlaysetInfoSection } from "../components/PlaysetInfoSection";
 import { ParkOtherInfoSection } from "../components/ParkOtherInfoSection";
 import { useLocation } from "react-router-dom";
+import { storage } from "../firebase/firebase";
+import { ref, getDownloadURL } from "firebase/storage";
 
 export const ParkDetailInfo = () => {
-  const location = useLocation();
+  const [image, setImage] = useState(null);
 
+  const location = useLocation();
   const detailnfo = location.state.info;
 
+  const parkId = detailnfo.id;
+  console.log("parkId:", parkId);
+  const getImageFunc = () => {
+    const gsReference = ref(
+      storage,
+      // "gs://park-app-2022.appspot.com/Vm2xdwSaPduEmsn0AcrO_main.jpg"
+      `gs://park-app-2022.appspot.com/${parkId}_main.jpg`
+    );
+    getDownloadURL(gsReference)
+      .then((url) => {
+        // console.log("URL:", url);
+        setImage(url);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getImageFunc();
+  }, []);
+
+  // console.log("image:", image);
   return (
     <div style={styles.body}>
       <Header />
       <div style={styles.section}>
-        {/* <img src={ParkImg} alt="メイン画像" style={styles.main} /> */}
+        {image && <img src={image} alt="メイン画像" style={styles.main} />}
         <ParkBasicInfoSection data={detailnfo} />
         <PlaysetInfoSection data={detailnfo} />
         <ParkOtherInfoSection other={detailnfo.other} />
